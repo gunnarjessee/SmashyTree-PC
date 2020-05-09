8a1ce91d9544369d83e0ac8c8c7838755b35c90a
Gunnar <gunnarjessee@gmail.com>
2020-05-08 22:32:46 -0400

    Added Event Manager
    Added setups for OnClick, OnTilePressed, OnPaused events, but no functionallity.

---
+++ Assets/GamePlay Logic/Scripts/EventManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public event EventHandler OnTilePressed;
+    public event EventHandler OnClick;
+    public event EventHandler OnPause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
