6bbc5db876bdc84976cdd442a79b6ea00384b453
Gunnar <gunnarjessee@gmail.com>
2020-05-21 18:04:07 -0400

    Trees added to GrassTilePrefab
    Togglable models for GrassTile so trees can be disabled and reenabled.

--- Assets/GamePlay Logic/Scripts/GrassTile.cs
+++ Assets/GamePlay Logic/Scripts/GrassTile.cs
@@ -3,21 +3,44 @@ using UnityEngine;
 
 public class GrassTile : MonoBehaviour
 {
+    public GameObject treeInstance;
 
     private Vector3 _position;
     private Vector3 _offsetPosition;
     private float offsetValue = .25f;
+
+    private bool hasTree = false;
+
     private void Start()
     {
         _position = transform.position;
         _offsetPosition = _position;
         _offsetPosition.y -= offsetValue;
+        treeInstance.SetActive(HasTree());
     }
 
     public void OnClick()
     {
         transform.position = _offsetPosition;
         StartCoroutine(ResetPosition());
+        ToggleTree();
+    }
+
+    public bool ToggleTree()
+    {
+        if (hasTree)
+        {
+            hasTree = false;
+        } else if (!hasTree) hasTree = true;
+
+        treeInstance.SetActive(hasTree);
+
+        return HasTree();
+    }
+
+    public bool HasTree()
+    {
+        return hasTree;
     }
 
     IEnumerator ResetPosition()
